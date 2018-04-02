e828591c04b47ba8e51842494ebb798253073772
Michelle <mtho@wpi.edu>
2018-04-02 11:49:01 -0400

    calendar

--- app/src/main/java/com/example/nag/productify/MainActivity.java
+++ app/src/main/java/com/example/nag/productify/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.nag.productify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button assign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        assign = (Button) findViewById(R.id.makeAssignBut);
+
+    }
+
+    public void assignNew (View view)
+    {
+        Intent assign = new Intent(MainActivity.this, Assignment.class);
+        startActivity(assign);
     }
 
 
